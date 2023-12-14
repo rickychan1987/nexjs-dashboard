2f91f0d43c2a21f2b09205130aecc7c60d665906
DESKTOP-BG25CL1\admin <van.la@paradiseio.com>
2023-12-14 20:14:00 +0700

    update title to SSK

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">天堂游戏</p>
+      <p className="text-[40px] text-center">S.S.K</p>
     </div>
   );
 }
